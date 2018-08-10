2a10c9d80d964cb390508c2675925c792f9ef8f9
Benjamin Winslett <winslett.benjamin@gmail.com>
2018-08-09 22:05:42 -0600

    commissions page complete

--- js/main.js
+++ js/main.js
@@ -1,60 +1,86 @@
-    function worksdropdownclick() {
-        document.getElementById("worksdropdown").classList.toggle("show");
-        if (document.getElementById("storedropdown").classList.contains('show')) {
-            document.getElementById("storedropdown").classList.remove('show');
+function worksdropdownclick() {
+    document.getElementById("worksdropdown").classList.toggle("show");
+    if (document.getElementById("storedropdown").classList.contains('show')) {
+        document.getElementById("storedropdown").classList.remove('show');
+    }
+}
+function storedropdownclick() {
+    document.getElementById("storedropdown").classList.toggle("show");
+    if (document.getElementById("worksdropdown").classList.contains('show')) {
+        document.getElementById("worksdropdown").classList.remove('show');
+    }
+}
+window.onclick = function(e) {
+    if (!e.target.matches('.navitem')) {
+        var worksDropdown = document.getElementById("worksdropdown");
+        var storeDropdown = document.getElementById("storedropdown");
+        if (worksDropdown.classList.contains('show')) {
+            worksDropdown.classList.remove('show');
+        }
+        if (storeDropdown.classList.contains('show')) {
+            storeDropdown.classList.remove('show');
         }
     }
-    function storedropdownclick() {
-        document.getElementById("storedropdown").classList.toggle("show");
-        if (document.getElementById("worksdropdown").classList.contains('show')) {
-            document.getElementById("worksdropdown").classList.remove('show');
-        }
+}
+function displayHiddenFields(selectedItem) {
+    var styleHidden = document.getElementById("style");
+    var otherHidden = document.getElementById("other");
+    $("#style-input").removeAttr("required");
+    $("#other_genre-input").removeAttr("required");
+    if (selectedItem.value == "invitation_genre" || selectedItem.value == ""){
+        styleHidden.style.display="none";
+        otherHidden.style.display="none";
+        SelectElement("style-input", "");
+        $("#other_genre-input").val("");
     }
-    window.onclick = function(e) {
-        if (!e.target.matches('.navitem')) {
-            var worksDropdown = document.getElementById("worksdropdown");
-            var storeDropdown = document.getElementById("storedropdown");
-            if (worksDropdown.classList.contains('show')) {
-                worksDropdown.classList.remove('show');
-            }
-            if (storeDropdown.classList.contains('show')) {
-                storeDropdown.classList.remove('show');
-            }
-        }
+    else if (selectedItem.value == "other_genre"){
+        styleHidden.style.display="block";
+        otherHidden.style.display="block";
+        $("#style-input").attr("required", "required");
+        $("#other_genre-input").attr("required", "required");
+    }  
+    else{
+        styleHidden.style.display="block";
+        otherHidden.style.display="none";
+        $("#style-input").attr("required", "required");
+        $("#other_genre-input").val("");
     }
-    
-    function showGenreDetail(selected) {
-        document.getElementById("fanart").style.display="none";
-        document.getElementById("cc").style.display="none";
-        document.getElementById("invitation").style.display="none";
-        document.getElementById("other").style.display="none";
+}
 
-        document.getElementById(selected).style.display="block";;
+function showGenreDetail(selected) {
+    var numGenres = document.getElementsByClassName("genreHideable").length;
+    document.getElementById("genreDetailTitle").style.display="none";
+    for (var i=0; i < numGenres; i++){
+        document.getElementsByClassName("genreHideable")[i].style.display = "none";
     }
-    function showStyleDetail(selected) {
-        document.getElementById("sketch_style").classList.remove("show");
-        document.getElementById("line_art_style").classList.remove("show");
-        document.getElementById("flat_color_style").classList.remove("show");
-        document.getElementById("cell_shading_style").classList.remove("show");
-        document.getElementById("lined_full_color_style").classList.remove("show");
-        document.getElementById("full_render_style").classList.remove("show");
+    if(selected.value != ""){
+        document.getElementById("genreDetailTitle").style.display="block";
+        document.getElementById(selected.value).style.display="block";
+    }
+}
 
-        document.getElementById(selected).classList.add("show");
+function showStyleDetail(selected) {
+    var numStyles = document.getElementsByClassName("styleHideable").length;
+    document.getElementById("styleDetailTitle").style.display="none";
+    for (var i=0; i < numStyles; i++){
+        document.getElementsByClassName("styleHideable")[i].style.display = "none";
     }
-    function displayHidden(selectedItem) {
-        console.log("Function is being called")
-        var styleHidden = document.getElementById("style");
-        var otherHidden = document.getElementById("other");
-        if (selectedItem.value == "invitation" || selectedItem.value == "none"){
-            styleHidden.style.display="none"
-            otherHidden.style.display="none";
-        }
-        else if (selectedItem.value == "other"){
-            styleHidden.style.display="block";
-            otherHidden.style.display="block";
-        }  
-        else{
-            styleHidden.style.display="block";
-            otherHidden.style.display="none";
-        }
+    if(selected.value != ""){
+        document.getElementById("styleDetailTitle").style.display="block";
+        document.getElementById(selected.value).style.display="block";
     }
+}
+var currPrice = 30.00;  
+$(".calculate").change(function() {
+    var newPrice = currPrice;
+    $(".calculate option:selected").each(function() {
+        newPrice += $(this).data('price')
+    });
+    $("#price").html(newPrice.toFixed(2));
+});
+
+function SelectElement(id, valueToSelect)
+{    
+    var element = document.getElementById(id);
+    element.value = valueToSelect;
+}
